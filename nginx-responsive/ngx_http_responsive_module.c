70ec9541c6b0c0ef7c30ef3a40a11d2e3ddc0c4c
Julien Stuyck <stuyck@gmail.com>
2016-02-19 17:33:55 +0000

    Add default conf to avoid frok on star, and enable debug mode

--- nginx-responsive/ngx_http_responsive_module.c
+++ nginx-responsive/ngx_http_responsive_module.c
@@ -1,75 +1,117 @@
+/*
+** ngx_http_responsive_module.c for nginx_responsive
+**
+** Made by Julien Stuyck
+** Login   jtsuyck@github
+**
+** Started on  18 Feb 2016
+** Last update 19 Feb 2016
+*/
+
 #include <ngx_config.h>
 #include <ngx_core.h>
 #include <ngx_http.h>
 
 #include "ngx_http_responsive_module.h"
 
-static u_char* ngx_responsive_string = (u_char*)"test";
 
-static char* ngx_http_responsive(ngx_conf_t* cf, ngx_command_t* cmd, void* conf)
+static ngx_int_t	ngx_http_responsive_init(ngx_conf_t *cf)
 {
-	ngx_http_core_loc_conf_t*	clcf;
+	ngx_http_handler_pt        *h;
+	ngx_http_core_main_conf_t  *cmcf;
 
-	clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
-	clcf->handler = ngx_http_responsive_handler;
+	cmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module);
 
-	return NGX_CONF_OK;
+	h = ngx_array_push(&cmcf->phases[NGX_HTTP_CONTENT_PHASE].handlers);
+	if (h == NULL) {
+		return NGX_ERROR;
+	}
+
+	*h = ngx_http_responsive_handler;
+
+	return NGX_OK;
 }
+
+
+static void*	ngx_http_responsive_create_loc_conf(ngx_conf_t*	cf)
+{
+	ngx_http_responsive_loc_conf_t  *conf;
+
+	conf = ngx_palloc(cf->pool, sizeof(ngx_http_responsive_loc_conf_t));
+	if (conf == NULL) {
+		return NULL;
+	}
+
+	conf->enable = NGX_CONF_UNSET;
+
+	return conf;
+}
+
 
 static ngx_int_t ngx_http_responsive_handler(ngx_http_request_t* r)
 {
-	ngx_int_t	rc;
-	ngx_buf_t*	b;
+	
 	ngx_chain_t	out;
+	
+	u_char	*last;
+	//u_char	*filename;
+	size_t	root;
 
-	if (!(r->method & (NGX_HTTP_GET|NGX_HTTP_HEAD)))
-	{
-		return NGX_HTTP_NOT_ALLOWED;
+	ngx_chain_t 					out;
+	ngx_str_t 						path;
+	ngx_buf_t						*b;
+	ngx_http_responsive_loc_conf_t 	*locconf;
+
+
+	if (r->uri.data[r->uri.len - 1] != '/') {
+		return NGX_DECLINED;
 	}
 
-	rc = ngx_http_discard_request_body(r);
-	if (rc != NGX_OK)
-	{
-		return rc;
+	if (!(r->method & (NGX_HTTP_GET|NGX_HTTP_HEAD))) {
+		return NGX_DECLINED;
 	}
 
-	r->headers_out.content_type_len = sizeof("text/html") - 1;
-	r->headers_out.content_type.len = sizeof("text/html") - 1;
-	r->headers_out.content_type.data = (u_char*)"text/html";
+	locconf = ngx_http_get_module_loc_conf(r, ngx_http_responsive_module);
 
-	if (r->method == NGX_HTTP_HEAD)
-	{
-		r->headers_out.status = NGX_HTTP_OK;
-		r->headers_out.content_length_n = sizeof(ngx_responsive_string) - 1;
-		return ngx_http_send_header(r);
+	if (!locconf->enable) {
+		return NGX_DECLINED;
 	}
 
-	b = ngx_pcalloc(r->pool, sizeof(ngx_buf_t));
-	if (b == NULL)
-	{
+	last = ngx_http_map_uri_to_path(r, &path, &root, 0);
+
+	if (last == NULL) {
 		return NGX_HTTP_INTERNAL_SERVER_ERROR;
 	}
 
+	ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "http responsive: \"%s\"", path.data);
+
+
+	b = ngx_http_responsive_compress(r, &path, 10,10);
+
+
+
 	out.buf = b;
-	out.next = NULL;
+    out.next = NULL;
 
-	b->pos = ngx_responsive_string;
-	b->last = ngx_responsive_string + sizeof(ngx_responsive_string) - 1;
-	b->memory = 1;
-	b->last_buf = 1;
+    r->headers_out.status = NGX_HTTP_OK;
 
-	r->headers_out.status = NGX_HTTP_OK;
-	r->headers_out.content_length_n = sizeof(ngx_responsive_string) - 1;
 
-	rc = ngx_http_send_header(r);
-
-	if (rc == NGX_ERROR || rc > NGX_OK || r->header_only)
-	{
-		return (rc);
-	}
 	return ngx_http_output_filter(r, &out);
 }
 
 
+
+static ngx_buf_t *ngx_http_responsive_compress(ngx_http_request_t *r,ngx_str_t path, int sizex, int sizey)
+{
+
+
+	b = ngx_create_temp_buf(r->pool, len);
+	if (b == NULL) {
+        return NULL;
+    }
+
+    b->last = ngx_cpymem(b->last, "</pre><hr>", sizeof("</pre><hr>") - 1);
+    return b;
+}
 
 
--- nginx-responsive/ngx_http_responsive_module.h
+++ nginx-responsive/ngx_http_responsive_module.h
@@ -1,29 +1,46 @@
+/*
+** ngx_http_responsive_module.h for nginx_responsive
+**
+** Made by Julien Stuyck
+** Login   jstuyck@github
+**
+** Started on  18 Feb 2016
+** Last update 19 Feb 2016
+*/
 
 #ifndef NGX_HTTP_RESPONSIVE_MODULE_H
 #define NGX_HTTP_RESPONSIVE_MODULE_H
 
-static char* ngx_http_responsive(ngx_conf_t* cf, ngx_command_t* cmd, void* conf);
-static ngx_int_t ngx_http_responsive_handler(ngx_http_request_t* r);
+
+
+static ngx_int_t	ngx_http_responsive_init(ngx_conf_t *cf);
+static void*		ngx_http_responsive_create_loc_conf(ngx_conf_t*	cf);
+static ngx_int_t 	ngx_http_responsive_handler(ngx_http_request_t* r);
+
+typedef struct {
+    ngx_flag_t     enable;
+} ngx_http_responsive_loc_conf_t;
+
 
 static ngx_command_t ngx_http_responsive_commands[] = {
 	{
 		ngx_string("responsive"),
-		NGX_HTTP_LOC_CONF|NGX_CONF_NOARGS,
-		ngx_http_responsive,
-		0,0,
-		NULL
-	},
+		NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_FLAG,
+     	ngx_conf_set_flag_slot,
+      	NGX_HTTP_LOC_CONF_OFFSET,
+      	offsetof(ngx_http_responsive_loc_conf_t, enable),
+      	NULL },
 	ngx_null_command
 };
 
 static ngx_http_module_t ngx_http_responsive_ctx = {
 	NULL,	//Preconfiguration
-	NULL,	//Postconfiguration
+	ngx_http_responsive_init,	//Postconfiguration
 	NULL,	//create main configuration
 	NULL,	//init main configuration
 	NULL,	//create server configuration
 	NULL,	//merge server configuration
-	NULL,	//create location configuration
+	ngx_http_responsive_create_loc_conf,	//create location configuration
 	NULL,	//merge location configuration
 };
 
